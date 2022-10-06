43dc192556c844b2d32c3435d25cbf3e25b0ec95
Heather Maiden <hthrmdn@gmail.com>
2022-10-06 14:48:05 -0500

    Adding HealthChecks

--- Program.cs
+++ Program.cs
@@ -4,18 +4,21 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Text.Json;
+using System.Net.Mime;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
+var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 
 builder.Services.AddSingleton<IMongoClient>(serviceProvider => 
 {
-    var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    return new MongoClient(settings.ConnectionString);
+    
+    return new MongoClient(mongoDbSettings.ConnectionString);
 });
 // Add services to the container.
 builder.Services.AddSingleton<IItemsRepository,MongoDbItemsRepository>();
@@ -25,6 +28,11 @@ builder.Services.AddControllers(options => {
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+    .AddMongoDb(mongoDbSettings.ConnectionString, 
+    name: "mongodb", 
+    timeout: TimeSpan.FromSeconds(3),
+    tags: new[] {"ready"});
 
 var app = builder.Build();
 
@@ -40,5 +48,30 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//https://localhost:5001/health/ready for status in postman
+app.MapHealthChecks("/health/ready", new HealthCheckOptions{
+    Predicate = (check) => check.Tags.Contains("ready"),
+    ResponseWriter = async(context, report) => 
+    {
+        var result = JsonSerializer.Serialize(
+            new{
+                status = report.Status.ToString(),
+                checks = report.Entries.Select(entry => 
+                new{
+                    name = entry.Key,
+                    status = entry.Value.Status.ToString(),
+                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                    duration = entry.Value.Duration.ToString()
+                } )
+            }
+        );
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        await context.Response.WriteAsync(result);
+    }
+});
+app.MapHealthChecks("/health/live", new HealthCheckOptions{
+    Predicate = (_) => false
+});
 
 app.Run();
